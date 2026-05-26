9edb232ca60e099f6651511720858dc1ad133d54
jawc-05 <joaoalfredowc@gmail.com>
2026-05-26 14:34:19 +0200

    feat: initial commit

---
+++ modPooJs/objects.js
@@ -0,0 +1,9 @@
+const carroJoão = {
+    modelo:'Fiesta',
+    fabricante: 'Ford',
+    anoMOdelo: 2020,
+    anoFabricação: 2019,
+    acelerar: function(){
+        console.log("vrummm")
+    }
+}
