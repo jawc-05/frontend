d8fbb3fe12dc7e9760e5dee2be10e148bf70a70b
jawc-05 <joaoalfredowc@gmail.com>
2026-05-26 14:43:11 +0200

    feat: adding basics examples of objects in js

--- modPooJs/objects.js
+++ modPooJs/objects.js
@@ -7,3 +7,27 @@ const carroJoão = {
         console.log("vrummm")
     }
 }
+const carroMaria = {
+    modelo:'Ka',
+    fabricante: 'Ford',
+    anoMOdelo: 2016,
+    anoFabricação: 2015,
+    acelerar: function(){
+        console.log("vrummm")
+    }
+}
+
+function Carro(modelo, fabricante, anoMOdelo, anoFabricação ){
+    this.modelo = modelo;
+    this.fabricante = fabricante;
+    this.anoMOdelo = anoMOdelo;
+    this.anoFabricação = anoFabricação;
+    this.acelerar = function(){
+        console.log("vrumm")
+    }
+}
+
+const carroDoJoão2 = new Carro("Fiesta", "Ford", 2020, 2019)
+const carroDaMaria2 = new Carro("Ka", "Ford", 2016, 2015)
+
+console.log(carroDoJoão2, carroDaMaria2)
